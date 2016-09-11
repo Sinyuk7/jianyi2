f07eca0471f7cebd1972d4b195bbb711ce31afcf
沈烨坷 <80998062@qq.com>
2016-09-11 10:57:15 +0800

    Add toolbar indicator

---
+++ app/src/main/java/com/sinyuk/jianyi/ui/need/NeedListFragment.java
@@ -0,0 +1,24 @@
+package com.sinyuk.jianyi.ui.need;
+
+import com.sinyuk.jianyi.R;
+import com.sinyuk.jianyi.ui.BaseFragment;
+
+/**
+ * Created by Sinyuk on 16/9/11.
+ */
+public class NeedListFragment extends BaseFragment{
+    @Override
+    protected void beforeInflate() {
+
+    }
+
+    @Override
+    protected int getRootViewId() {
+        return R.layout.fragment_need_list;
+    }
+
+    @Override
+    protected void finishInflate() {
+
+    }
+}
