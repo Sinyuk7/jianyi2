b32283f2e209387b96b1711932868844401fbba7
沈烨坷 <80998062@qq.com>
2016-09-12 11:55:43 +0800

    Create player layout

---
+++ app/src/main/java/com/sinyuk/jianyi/ui/player/PlayerActivity.java
@@ -0,0 +1,27 @@
+package com.sinyuk.jianyi.ui.player;
+
+import android.os.Bundle;
+
+import com.sinyuk.jianyi.R;
+import com.sinyuk.jianyi.ui.BaseActivity;
+
+/**
+ * Created by Sinyuk on 16/9/12.
+ */
+public class PlayerActivity extends BaseActivity{
+
+    @Override
+    protected int getContentViewID() {
+        return R.layout.activity_player;
+    }
+
+    @Override
+    protected void beforeInflating() {
+
+    }
+
+    @Override
+    protected void finishInflating(Bundle savedInstanceState) {
+
+    }
+}
